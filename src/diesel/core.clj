bb48e39401a68efaad867486f77b24bff8f0fa3c
Alex Bahouth <abahouth@onekingslane.com>
2013-12-22 20:38:17 -0800

    refactoring cleanup check point

--- src/diesel/core.clj
+++ src/diesel/core.clj
@@ -1,21 +1,24 @@
 (ns diesel.core
+  "A small _diesel_ engine to build your own DSLs!"
+  {:author "Alex Bahouth"
+   :date "12/22/2013"}
   (:use [roxxi.utils.print]))
 
-(defn of-form? [fn-or-op expr]
+(defn- of-form? [fn-or-op expr]
   (if (fn? fn-or-op)
     (fn-or-op expr)
     (and (list? expr)
          (not (empty? expr))
          (= (first expr) fn-or-op))))
 
-(defn remove-dispatch-form-cruft
+(defn- remove-dispatch-form-cruft
   "Our dispatch forms have `=>` to make them pretty.
 Since that isn't valid syntax, this removes that value
 and effectively turns `[a => b]` into `[a b]`."
   [dispatch-expr]
   [(first dispatch-expr) (last dispatch-expr)])
 
-(defn ordered-expr-interp [expr dispatch-mappings]
+(defn- ordered-expr-interp [expr dispatch-mappings]
    (loop [op-fns=>ds dispatch-mappings]
     (when op-fns=>ds
       (let [op-fn=>d (first op-fns=>ds)
@@ -42,46 +45,3 @@ and effectively turns `[a => b]` into `[a b]`."
        (defmethod ~name :unknown-operator ~expr+formals
          (str "Unknown handler for `" ~'expr
               "` when handling `" ~'expr "`")))))
-
-(def special-ops #{'+ '- '/ '*})
-
-(defn special-op? [expr]
-  (special-ops (first expr)))
-
-(macroexpand-1
-'(definterp my-interp []
-   ['add => :add]
-   ['sub => :sub]
-   [special-op? => :special-op]))
-
-(definterp my-interp []
-  ['add => :add]
-  ['sub => :sub]
-  ['div => :div]
-  [special-op? => :special-op])
-
-(defmethod my-interp :add [[_ & args]]
-  (apply +  (map my-interp args)))
-
-(defmethod my-interp :sub [[_ & args]]
-  (if (= (count args) 1)
-    (my-interp (first args))
-    (apply - (map my-interp args))))
-
-(defmethod my-interp :div [[_ dividend divisor]]
-  (/ dividend divisor))
-
-(defmethod my-interp :special-op [[op & args]]
-  (if (= (count args) 1)
-    (my-interp (first args))
-    (apply @(resolve op) (map my-interp args))))
-
-(def x 10)
-(my-interp '(add 6 7))
-(my-interp '(add 6 x))
-
-(my-interp '(+ 25 (add 6 (sub 10 x))))
-
-(my-interp '(add 26))
-
-(my-interp '(div 10 20))
--- test/diesel/core_test.clj
+++ test/diesel/core_test.clj
@@ -5,3 +5,42 @@
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 0 1))))
+
+
+(def special-ops #{'+ '- '/ '*})
+
+(defn special-op? [expr]
+  (special-ops (first expr)))
+
+(definterp my-interp []
+  ['add => :add]
+  ['sub => :sub]
+  ['div => :div]
+  [special-op? => :special-op])
+
+(defmethod my-interp :add [[_ & args]]
+  (apply +  (map my-interp args)))
+
+(defmethod my-interp :sub [[_ & args]]
+  (if (= (count args) 1)
+    (my-interp (first args))
+    (apply - (map my-interp args))))
+
+(defmethod my-interp :div [[_ dividend divisor]]
+  (/ dividend divisor))
+
+(defmethod my-interp :special-op [[op & args]]
+  (if (= (count args) 1)
+    (my-interp (first args))
+    (apply @(resolve op) (map my-interp args))))
+
+
+(def x 10)
+(my-interp '(add 6 7))
+(my-interp '(add 6 x))
+
+(my-interp '(+ 25 (add 6 (sub 10 x))))
+
+(my-interp '(add 26))
+
+(my-interp '(div 10 20))
