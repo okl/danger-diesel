a92994635df4a819cde73b5a7b14d0e1548356f3
Matt Halverson <mhalverson@onekingslane.com>
2014-03-11 17:40:23 -0700

    Up the version

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject com.onekingslane.danger/diesel "1.0.1"
+(defproject com.onekingslane.danger/diesel "1.0.2"
   :description "Diesel! The DSL building engine!"
   :url "http://www.github.com/okl/danger-diesel"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.onekingslane.danger/clojure-common-utils "0.0.18"]])
+                 [com.onekingslane.danger/clojure-common-utils "0.0.22"]])
